0d83916085fa1fc0a5ebca08cda88e0e9e9ac8e6
Axel <nigthmaresans2@gmail.com>
2025-02-23 01:15:52 -0600

    test

--- lua/pywal16/core.lua
+++ lua/pywal16/core.lua
@@ -1,29 +1,30 @@
 local M = {}
 
 function M.get_colors()
-  vim.cmd [[ source $HOME/.cache/wal/colors-wal.vim ]]
+  --vim.cmd [[ source $HOME/.config/nvim/lua/themes/colors.vim ]]
+  local _colors = require("config._colors")
 
   return {
     transparent = "NONE",
-    background = vim.g.background,
-    foreground = vim.g.foreground,
-    cursor = vim.g.cursor,
-    color0 = vim.g.color0,
-    color1 = vim.g.color1,
-    color2 = vim.g.color2,
-    color3 = vim.g.color3,
-    color4 = vim.g.color4,
-    color5 = vim.g.color5,
-    color6 = vim.g.color6,
-    color7 = vim.g.color7,
-    color8 = vim.g.color8,
-    color9 = vim.g.color9,
-    color10 = vim.g.color10,
-    color11 = vim.g.color11,
-    color12 = vim.g.color12,
-    color13 = vim.g.color13,
-    color14 = vim.g.color14,
-    color15 = vim.g.color15,
+    background = _colors.background,
+    foreground = _colors.foreground,
+    cursor = _colors.cursor,
+    color0 = _colors.color0,
+    color1 = _colors.color1,
+    color2 = _colors.color2,
+    color3 = _colors.color3,
+    color4 = _colors.color4,
+    color5 = _colors.color5,
+    color6 = _colors.color6,
+    color7 = _colors.color7,
+    color8 = _colors.color8,
+    color9 = _colors.color9,
+    color10 = _colors.color10,
+    color11 = _colors.color11,
+    color12 = _colors.color12,
+    color13 = _colors.color13,
+    color14 = _colors.color14,
+    color15 = _colors.color15,
   }
 end
 
